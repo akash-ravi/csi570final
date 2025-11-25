249eb7e8942b85517340d01203767cd94a8162ee
XciciciX <93366526+XciciciX@users.noreply.github.com>
2025-11-25 00:27:29 -0800

    update run with two modes

--- Sequence_Alignment/run_tests.sh
+++ Sequence_Alignment/run_tests.sh
@@ -2,12 +2,22 @@
 
 # Directory containing the test cases
 TEST_DIR="../CSCI570_Project/SampleTestCases"
+RESULTS_DIR="../CSCI570_Project/TestResults"
+
+# create results dir if not exists
+mkdir -p "$RESULTS_DIR"
+
+mkdir -p "$RESULTS_DIR/basic"
+mkdir -p "$RESULTS_DIR/efficient"
 
 # Python program to test
-PROGRAM="python3 basic.py"
+PROGRAM_BASIC="python3 basic.py"
+PROGRAM_EFFICIENT="python3 efficient.py"
 
 echo "===== Running Test Cases ====="
 
+# Basic Tests
+echo "---- Basic Tests ----"
 # Loop through input files
 for input in $TEST_DIR/input*.txt; do
     # Extract the number (e.g., input3 → 3)
@@ -16,12 +26,43 @@ for input in $TEST_DIR/input*.txt; do
     num=${num//.txt/}
 
     expected="$TEST_DIR/output${num}.txt"
-    actual="$TEST_DIR/my_output${num}.txt"
+    actual="$RESULTS_DIR/basic/output${num}.txt"
 
     echo -n "Test $num: "
 
      # Run program
-    $PROGRAM "$input" "$actual"
+    $PROGRAM_BASIC "$input" "$actual"
+
+    # extract first line of expected and actual
+    expected_first=$(sed -n '1p' "$expected")
+    actual_first=$(sed -n '1p' "$actual")
+
+    if [ "$expected_first" = "$actual_first" ]; then
+        echo "PASS"
+    else
+        echo "FAIL ❌"
+        echo "  Expected first line: $expected_first"
+        echo "  Actual first line:   $actual_first"
+    fi
+done
+
+
+# Efficient Tests
+echo "---- Efficient Tests ----"
+# Loop through input files
+for input in $TEST_DIR/input*.txt; do
+    # Extract the number (e.g., input3 → 3)
+    base=$(basename "$input")
+    num=${base//input/}
+    num=${num//.txt/}
+
+    expected="$TEST_DIR/output${num}.txt"
+    actual="$RESULTS_DIR/efficient/output${num}.txt"
+
+    echo -n "Test $num: "
+
+     # Run program
+    $PROGRAM_EFFICIENT "$input" "$actual"
 
     # extract first line of expected and actual
     expected_first=$(sed -n '1p' "$expected")
